a4b8b8a170b8f7a4c08fbc0d998f2535eb458289
cballesteros <cristian.ballesteros@correounivalle.edu.co>
2016-09-15 15:42:00 -0500

    Merge Main Menu - Kinect Prototype 1

---
+++ Playtherapy/Assets/Scripts/PointUpdater.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace MovementDetectionLibrary
+{
+    public class PointUpdater : MonoBehaviour
+    {
+        public GameObject FullBodyObject;
+        private FullBody _FullBodyObject;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (FullBodyObject == null)
+            {
+                return;
+            }
+
+            _FullBodyObject = FullBodyObject.GetComponent<FullBody>();
+            if (_FullBodyObject == null)
+            {
+                return;
+            }
+
+            BodyPointPosition pos = _FullBodyObject.ReturnPointPosition((BodyParts)Enum.Parse(typeof(BodyParts), gameObject.transform.name.ToString()));
+            transform.position = new Vector3(pos.x, pos.y, pos.z);
+            
+            //Debug.Log("Elbow: x:"+pos.x + ", " + pos.y + "" + pos.z);
+        }
+    }
+}
+
