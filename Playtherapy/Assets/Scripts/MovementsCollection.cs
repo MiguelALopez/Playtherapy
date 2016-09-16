de0175094a6044f6f04ca2fc2bf31f372f15bb28
cballesteros <cristian.ballesteros@correounivalle.edu.co>
2016-09-15 23:13:52 -0500

    Instancia de movimientos, hasta codo

--- Playtherapy/Assets/Scripts/MovementsCollection.cs
+++ Playtherapy/Assets/Scripts/MovementsCollection.cs
@@ -16,18 +16,141 @@ namespace MovementDetectionLibrary
 
 		}
 
-        void headMovements()
+        public double headLateralAngle()
         {
 
+            // lateral flexion
+            BodyPointPosition pointOne = bodyPointsCollection[BodyParts.SpineShoulder].getCurrentPosition();
+            BodyPointPosition pointCenter = bodyPointsCollection[BodyParts.Neck].getCurrentPosition();
+            BodyPointPosition pointTwo = bodyPointsCollection[BodyParts.Head].getCurrentPosition();
+            Movement headMovementFlex = new Movement();
+
+            return headMovementFlex.getAngleJoints(pointOne, pointCenter, pointTwo);
         }
-        void shoulderRigthMovements()
+
+        public double headFrontalExtAngle()
         {
 
+            //  frontal extension 
+
+            BodyPointPosition pointOne = bodyPointsCollection[BodyParts.SpineShoulder].getCurrentPosition();
+            BodyPointPosition pointCenter = bodyPointsCollection[BodyParts.Neck].getCurrentPosition(); 
+            BodyPointPosition pointTwo = bodyPointsCollection[BodyParts.Head].getCurrentPosition();
+            Movement headMovementFlex = new Movement();
+
+            return headMovementFlex.getAngleJoints(pointOne, pointCenter, pointTwo);
         }
-        void elbowMovements()
+
+
+        public double headFrontalFleAngle()
         {
 
+            //  frontal flexion 
+
+            BodyPointPosition pointOne = bodyPointsCollection[BodyParts.SpineShoulder].getCurrentPosition();
+            BodyPointPosition pointCenter = bodyPointsCollection[BodyParts.Neck].getCurrentPosition();
+            BodyPointPosition pointTwo = bodyPointsCollection[BodyParts.Head].getCurrentPosition();
+            Movement headMovementFlex = new Movement();
+
+            return headMovementFlex.getAngleJoints(pointOne, pointCenter, pointTwo);
         }
+
+    
+        public double shoulderAbdRigthMovements()
+        {
+
+            
+            BodyPointPosition pointCenter = bodyPointsCollection[BodyParts.ShoulderRight].getCurrentPosition();
+            BodyPointPosition pointTwo = bodyPointsCollection[BodyParts.ElbowRight].getCurrentPosition();
+            BodyPointPosition pointOne = pointCenter;
+            pointOne.y = pointOne.y - 0.2f;
+
+            Movement headMovementFlex = new Movement();
+
+            return headMovementFlex.getAngleJoints(pointOne, pointCenter, pointTwo);
+
+        }
+
+
+        public double shoulderFlexRigthMovements()
+        {
+
+
+            BodyPointPosition pointCenter = bodyPointsCollection[BodyParts.ShoulderRight].getCurrentPosition();
+            BodyPointPosition pointTwo = bodyPointsCollection[BodyParts.ElbowRight].getCurrentPosition();
+            BodyPointPosition pointOne = pointCenter;
+            pointOne.y = pointOne.y - 0.2f;
+
+            Movement headMovementFlex = new Movement();
+
+            return headMovementFlex.getAngleJoints(pointOne, pointCenter, pointTwo);
+
+        }
+
+
+        public double shoulderExtRigthMovements()
+        {
+
+
+            BodyPointPosition pointCenter = bodyPointsCollection[BodyParts.ShoulderRight].getCurrentPosition();
+            BodyPointPosition pointTwo = bodyPointsCollection[BodyParts.ElbowRight].getCurrentPosition();
+            BodyPointPosition pointOne = pointCenter;
+            pointOne.y = pointOne.y - 0.2f;
+
+            Movement headMovementFlex = new Movement();
+
+            return headMovementFlex.getAngleJoints(pointOne, pointCenter, pointTwo);
+
+        }
+
+
+
+        public double shoulderRotIntRigthMovements()
+        {
+
+
+            BodyPointPosition pointCenter = bodyPointsCollection[BodyParts.ElbowRight].getCurrentPosition();
+            BodyPointPosition pointTwo = bodyPointsCollection[BodyParts.WristRight].getCurrentPosition();
+            BodyPointPosition pointOne = pointCenter;
+            pointOne.z = pointOne.z - 0.2f;
+
+            Movement headMovementFlex = new Movement();
+
+            return headMovementFlex.getAngleJoints(pointOne, pointCenter, pointTwo);
+
+        }
+
+        public double shoulderRotExtRigthMovements()
+        {
+
+
+            BodyPointPosition pointCenter = bodyPointsCollection[BodyParts.ElbowRight].getCurrentPosition();
+            BodyPointPosition pointTwo = bodyPointsCollection[BodyParts.WristRight].getCurrentPosition();
+            BodyPointPosition pointOne = pointCenter;
+            pointOne.z = pointOne.z - 0.2f;
+
+            Movement headMovementFlex = new Movement();
+
+            return headMovementFlex.getAngleJoints(pointOne, pointCenter, pointTwo);
+
+        }
+
+        public double elbowFleExtMovement()
+        {
+
+            // Flexion
+
+            BodyPointPosition pointCenter = bodyPointsCollection[BodyParts.ElbowRight].getCurrentPosition();
+            BodyPointPosition pointTwo = bodyPointsCollection[BodyParts.WristRight].getCurrentPosition();
+            BodyPointPosition pointOne = bodyPointsCollection[BodyParts.ShoulderRight].getCurrentPosition();
+
+            Movement headMovementFlex = new Movement();
+
+            return headMovementFlex.getAngleJoints(pointOne, pointCenter, pointTwo);
+
+        }
+
+
         void wristMovements()
         {
 
