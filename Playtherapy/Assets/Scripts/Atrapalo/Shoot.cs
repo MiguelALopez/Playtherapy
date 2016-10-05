93eb1bebab32abcb15065066c1ffe983d179d930
fermat986 <martinez.alvaro@correounivalle.edu.co>
2016-10-05 16:05:16 -0500

    Creacion de escena para el juego, funcion para disparar la pelota

---
+++ Playtherapy/Assets/Scripts/Atrapalo/Shoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.GetComponent<Rigidbody>().AddForce(10, 50, 0, ForceMode.Acceleration);
+        }
+	
+	}
+}
--- Playtherapy/Assets/Scripts/Main Menu/GameManager.cs
+++ Playtherapy/Assets/Scripts/Main Menu/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
+
+        
 	
 	}
 }
