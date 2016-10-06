5aca6cee28033afa6378eb3e6ade87e2d37dbdba
fermat986 <martinez.alvaro@correounivalle.edu.co>
2016-10-06 17:28:07 -0500

    Create a function to shoot the ball to the player

--- Playtherapy/Assets/Scripts/Atrapalo/Shoot.cs
+++ Playtherapy/Assets/Scripts/Atrapalo/Shoot.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class Shoot : MonoBehaviour {
+    
+
+    int uno = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -11,10 +14,38 @@ public class Shoot : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (uno == 0)
         {
-            this.GetComponent<Rigidbody>().AddForce(10, 50, 0, ForceMode.Acceleration);
+            float t = 3f;            
+
+            this.GetComponent<Rigidbody>().AddForce(calcularVectorVelocidad(t), ForceMode.VelocityChange);
+            uno = 1;
         }
 	
 	}
+
+    Vector3 calcularVectorVelocidad(float t)
+    {
+        Vector3 velIni = new Vector3();
+
+        float Vx = 0;
+        float Vy = 0;
+        float Vz = 0;
+        float X0 = this.transform.position.x;
+        float Z0 = this.transform.position.z;
+        float X1 = GameObject.FindGameObjectWithTag("Player").transform.position.x;
+        float Z1 = GameObject.FindGameObjectWithTag("Player").transform.position.z;
+
+        Vx = (X1 - X0) / t;
+        Vz = (Z1 - Z0) / t;
+
+        Vy = (9.8f * t) / 2;
+
+
+        velIni.x = Vx;
+        velIni.y = Vy;
+        velIni.z = Vz;
+
+        return velIni;
+    }
 }
