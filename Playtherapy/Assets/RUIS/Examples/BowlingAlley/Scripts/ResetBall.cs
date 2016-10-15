649528f5ea491018b5373704fd1e577dfa1578e6
fermat986 <martinez.alvaro@correounivalle.edu.co>
2016-10-14 19:58:23 -0500

    Kinect Avatars Attempt

---
+++ Playtherapy/Assets/RUIS/Examples/BowlingAlley/Scripts/ResetBall.cs
@@ -0,0 +1,37 @@
+/*****************************************************************************
+
+Content    :   Functionality to reset the ball position
+Authors    :   Mikael Matveinen, Tuukka Takala
+Copyright  :   Copyright 2016 Tuukka Takala, Mikael Matveinen. All Rights reserved.
+Licensing  :   RUIS is distributed under the LGPL Version 3 license.
+
+******************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class ResetBall : MonoBehaviour {
+//    public RUISPSMoveWand controller;
+	public RUISOpenVrWand controller;
+    public Transform ballResetSpot;
+
+    private bool shouldResetBall = true;
+
+    void FixedUpdate()
+    {
+		if(shouldResetBall || (controller && controller.menuButtonWasPressed)) //controller.moveButtonWasPressed)
+        {
+            transform.position = ballResetSpot.transform.position;
+            transform.rotation = ballResetSpot.transform.rotation;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+            shouldResetBall = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        shouldResetBall = true;
+    }
+}
