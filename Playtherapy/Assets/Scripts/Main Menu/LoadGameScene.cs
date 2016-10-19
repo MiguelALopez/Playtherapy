73eca3887385d616e40f557a21129faa93833e8e
Camilo1090 <camilo.ruiz.casanova@correounivalle.edu.co>
2016-10-19 01:22:18 -0500

    added object persistence and scene loading

---
+++ Playtherapy/Assets/Scripts/Main Menu/LoadGameScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadGameScene : MonoBehaviour
+{
+    public void Load()
+    {
+        string sceneName = this.gameObject.GetComponentInChildren<Text>().text;
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        Debug.Log("Tiki");
+    }
+}
