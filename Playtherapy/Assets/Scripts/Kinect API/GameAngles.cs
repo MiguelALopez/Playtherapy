1ba24f02b2dd4e83b588f05debaa12b5790e838c
fermat986 <martinez.alvaro@correounivalle.edu.co>
2016-10-24 14:16:26 -0500

    rreglo de merge

--- Playtherapy/Assets/Scripts/Kinect API/GameAngles.cs
+++ Playtherapy/Assets/Scripts/Kinect API/GameAngles.cs
@@ -24,7 +24,7 @@ namespace MovementDetectionLibrary
 
 			//Debug.Log ("magnitude" + (pointTwo - pointOne).magnitude);
 
-<<<<<<< HEAD
+
 
 
             pointFin.z = Mathf.Cos(angle) * (pointTwo - pointOne).magnitude*1.2f;
@@ -38,11 +38,7 @@ namespace MovementDetectionLibrary
             {
                 pointFin.z -= 1.5f;
             }
-=======
-			Debug.Log ("angulo cos sin "+angle);
-            pointFin.z = Mathf.Cos(angle) * (pointTwo - pointOne).magnitude ;
-            pointFin.y = Mathf.Sin(angle) * (pointTwo - pointOne).magnitude;
->>>>>>> 193d5be7f5b1d5c158066c848648f3a58adf97b2
+
 
             pointFin = pointFin + pointOne;
 
@@ -75,15 +71,10 @@ namespace MovementDetectionLibrary
                 float angle = (float)(arrayAngles[position]);
                 arrayAngles.RemoveAt(position);
 
-<<<<<<< HEAD
                 float angleRad = Mathf.Deg2Rad * angle;
 
                 return angleRad-Mathf.PI/2;
-=======
-                float angleRad = Mathf.Deg2Rad * Random.Range (-angle, 0);
-                Debug.Log("angulo rad" + angleRad);
-				return -Mathf.PI/6;
->>>>>>> 193d5be7f5b1d5c158066c848648f3a58adf97b2
+
 			} else {
 
                 int position = Mathf.RoundToInt(Random.Range(0, arrayAngles.Count));
@@ -93,11 +84,9 @@ namespace MovementDetectionLibrary
 
                 float angleRad = Mathf.Deg2Rad * angle;
 
-<<<<<<< HEAD
+
                 return 3 * Mathf.PI / 2 - angleRad;
-=======
-                return Mathf.PI/6;
->>>>>>> 193d5be7f5b1d5c158066c848648f3a58adf97b2
+
 			}
 
         }
