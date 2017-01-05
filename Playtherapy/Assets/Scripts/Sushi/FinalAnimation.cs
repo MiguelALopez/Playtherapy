76d964fb8c0437cf8cab124f8a91101508482036
Juan Diego Prado <juan.d.prado@correounivalle.edu.co>
2017-01-05 00:00:12 -0500

    Camera mover script added to Sushi

---
+++ Playtherapy/Assets/Scripts/Sushi/FinalAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalAnimation : MonoBehaviour {
+
+    GameObject cam;
+    GameObject table;
+
+	// Use this for initialization
+	void Start () {
+        cam = GameObject.Find("Main Camera");
+        table = GameObject.Find("SushiContainer");
+        cam.transform.Translate(table.transform.position - cam.transform.position - new Vector3(0.0f, 0.0f, 2.0f));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cam.transform.Translate(new Vector3(0.0f, 0.2f * Time.deltaTime, 0.0f));
+        
+	}
+}
