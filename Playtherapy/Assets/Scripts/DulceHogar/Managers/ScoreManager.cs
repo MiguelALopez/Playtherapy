4962b9a96aa44c03c9ea76f06477e078dd1f348f
mauroctm <mauroctm7@gmail.com>
2017-04-16 10:38:44 -0500

    Revert "Sonido Y cambio de Escena"
    This reverts commit 90b5a70280c4168c276587e0543c33beb41fcbb3.

--- Playtherapy/Assets/Scripts/DulceHogar/Managers/ScoreManager.cs
+++ Playtherapy/Assets/Scripts/DulceHogar/Managers/ScoreManager.cs
@@ -1,15 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 namespace CompleteProject
 {
     public class ScoreManager : MonoBehaviour
     {
         public static int score;        // The player's score.
-		public int nextLevel = 50; //Puntaje necesario para cambiar de nivel
-		private string nameScene;
+
 
         Text text;                      // Reference to the Text component.
 
@@ -18,10 +16,8 @@ namespace CompleteProject
         {
             // Set up the reference.
             text = GetComponent <Text> ();
-			Scene scene = SceneManager.GetActiveScene ();
-			nameScene = scene.name;
-			NGUIDebug.Log ("Escena Activa + " + nameScene + " ");
-			// Reset the score.
+
+            // Reset the score.
             score = 0;
         }
 
@@ -30,15 +26,6 @@ namespace CompleteProject
         {
             // Set the displayed text to be the word "Score" followed by the score value.
             text.text = "Score: " + score;
-			//Combio de Escena
-			if (score >= nextLevel && "Urban".Equals(nameScene)) {
-				print ("Cambio de Nivel");
-				SceneManager.LoadScene ("ParkNatural");
-			}
-			if (score >= nextLevel && "ParkNatural".Equals(nameScene)) {
-				print ("Cambio de Nivel");
-				SceneManager.LoadScene ("ParkNaturalDark");
-			}
         }
     }
 }
