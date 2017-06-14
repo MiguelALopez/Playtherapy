2f16e6d412fa5674c66a011b06ab27b218434bbb
Miguel Angel Lopez <miguel.angel.lopez@correounivalle.edu.co>
2017-06-13 21:36:58 -0500

    Added force field animation, tutorial panel and enemy

---
+++ Playtherapy/Assets/Scripts/Space/DroidBehavior.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DroidBehavior : MonoBehaviour {
+
+    public float horizontalSpeed = 10f;                     // Velocity of the horizontal move
+    public float tilt = 5f;                                 // Max rotation of the droid
+    public float rotateSpeed = 5f;                          // Velocity of the rotation
+    public float timeBetweenMove = 6f;                      //
+    public float timeBetweenFire = 2f;                      //
+    private float savedTime;
+    private float movementTime;
+    private float randomMove;
+
+    public Boundary boundary;                               // Boundaries of the ship movement
+
+    private Rigidbody m_rigidbody;                          // Rigidbody of the Ship
+    //private float moveHorizontal;                           // Amount of horizontal movement
+
+
+
+    // Use this for initialization
+    void Start () {
+        m_rigidbody = GetComponent<Rigidbody>();
+        ResetMovement();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (GameManagerSpace.gms.IsPlaying())
+        {
+            if(Time.time - savedTime >= timeBetweenMove)
+            {
+                HorizontalMove(randomMove);
+                if(Time.time - savedTime >= timeBetweenMove + movementTime)
+                {
+                    ResetMovement();
+                    HorizontalMove(0);
+                }
+            }
+            CalculateRotation();
+            CalculateBoundary();
+        }
+    }
+
+    /// <summary>
+    /// Calculates the horizontal move of the ship
+    /// </summary>
+    public void HorizontalMove(float moveHorizontal)
+    {
+        //moveHorizontal = Input.GetAxis("Horizontal");
+        m_rigidbody.velocity = new Vector3(moveHorizontal * horizontalSpeed, 0.0f, m_rigidbody.velocity.z);
+    }
+
+    /// <summary>
+    /// Calculates the effect of the ship rotation
+    /// </summary>
+    public void CalculateRotation()
+    {
+        transform.rotation = Quaternion.Slerp(m_rigidbody.rotation,
+            Quaternion.Euler(m_rigidbody.rotation.x, m_rigidbody.rotation.y, m_rigidbody.velocity.x * -tilt),
+            rotateSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Limist the border of the ship movement
+    /// </summary>
+    public void CalculateBoundary()
+    {
+        m_rigidbody.position = new Vector3
+            (
+            Mathf.Clamp(m_rigidbody.position.x, boundary.xMin, boundary.xMax),
+            m_rigidbody.position.y,
+            m_rigidbody.position.z
+            );
+    }
+
+    private void OnEnable()
+    {
+        savedTime = Time.time;
+    }
+
+    public void ResetMovement()
+    {
+        savedTime = Time.time;
+        movementTime = Random.Range(1, 2);
+        randomMove = Random.Range(-1, 2);
+        Debug.Log("Reset move " + randomMove);
+    }
+}
--- Playtherapy/Assets/Scripts/Space/ForceFieldDestroy.cs
+++ Playtherapy/Assets/Scripts/Space/ForceFieldDestroy.cs
@@ -6,9 +6,11 @@ public class ForceFieldDestroy : MonoBehaviour {
 
     public float lifeTime = 10f;                // The lifetime of the force field
     private float savedTime;                    // Temporal variable for save the init of the life
+    private Animator animation;
 
 	// Use this for initialization
 	void Start () {
+        animation = GetComponent<Animator>();
         savedTime = Time.time;
 	}
 	
@@ -17,7 +19,8 @@ public class ForceFieldDestroy : MonoBehaviour {
 
 		if(Time.time - savedTime >= lifeTime)
         {
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
+            StartCoroutine(Deactivate());
         }
 	}
 
@@ -25,5 +28,13 @@ public class ForceFieldDestroy : MonoBehaviour {
     private void OnEnable()
     {
         savedTime = Time.time;
+        animation.Play("ForceField", 0);
+    }
+
+    private IEnumerator Deactivate()
+    {
+        animation.Play("ForceFieldDeactivate", 0);
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
     }
 }
